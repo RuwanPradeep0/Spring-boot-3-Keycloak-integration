139c8924b621a913b179d78981c5396061dc27f5
Ruwan Pradeep <ruwanpradeep9911@gmail.com>
2024-07-06 23:15:12 +0530

    token converter

--- Keyclock/src/main/java/com/ruwan/Keyclock/DemoController.java
+++ Keyclock/src/main/java/com/ruwan/Keyclock/DemoController.java
@@ -1,5 +1,6 @@
 package com.ruwan.Keyclock;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo")
 public class DemoController {
     @GetMapping
+    @PreAuthorize("hasRole('client_user')")
     public String hello(){
         return "hello from spring boot and keyclock";
     }
 
     @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('client_admin')")
     public String hello2(){
         return "hello from spring boot and keyclock - Admin";
     }
--- Keyclock/src/main/java/com/ruwan/Keyclock/SecurityConfig.java
+++ Keyclock/src/main/java/com/ruwan/Keyclock/SecurityConfig.java
@@ -1,31 +1,39 @@
 package com.ruwan.Keyclock;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest()
-                        .authenticated())
-
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults()))
-
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthConverter)
+                        )
+                )
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        .sessionCreationPolicy(STATELESS)
+                );
 
         return http.build();
     }
